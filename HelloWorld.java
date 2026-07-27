14b1ba3994c98b12536322381f4d6bef1328fb0b
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-27 11:19:04 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+class HelloWorld{
+    
+    public static void main(){
+        x x = new x();
+        x.box();
+
+    }
+
+
+
+
+}
+    
---
+++ x.java
@@ -0,0 +1,7 @@
+class x{
+    public void box(){
+        System.out.println("\033c\033[47;30m\ninvoke box ");
+
+    }
+
+}
