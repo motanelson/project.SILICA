6836976665213e408238259f6ee118399da732da
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-28 17:16:48 +0100

    Add files via upload

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ class HelloWorld{
     
     public static void main(){
         x x = new x();
-        x.box(0,0,10,10);
+        x.box(10,2,10,10);
 
     }
 
--- x.java
+++ x.java
@@ -1,14 +1,16 @@
 class x{
     public void box(int xx,int y,int w,int h){
-        System.out.println("\033c\033[47;30m\ninvoke box \nx:");
-        System.out.println(xx);
-        System.out.println("\033[47;30m\ny:");
-        System.out.println(y);
-        System.out.println("\033[47;30m\nw:");
-        System.out.println(w);
-        System.out.println("\033[47;30m\nh:");
-        System.out.println(h);
-        System.out.println("\033[47;30m\n");
+        String xxx="\033[40;30m";
+        String www="\033[47;30m";
+        int x1=0;
+        for(x1=0;x1<xx;x1++)xxx=xxx+" ";
+        for(x1=0;x1<w;x1++)www=www+" ";
+        www=xxx+www;
+        System.out.println("\033c\033[47;30m\ninvoke box \n");
+        for(x1=0;x1<y;x1++)System.out.println("");
+        for(x1=0;x1<w;x1++)System.out.println(www);
+            
+        
 
 
     }
