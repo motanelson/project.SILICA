39f206446bcb70069f0abfbcb9fb6c488452fd1e
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-07-29 16:50:15 +0100

    Add files via upload

--- x.java
+++ x.java
@@ -3,12 +3,12 @@ class x{
         String xxx="\033[40;30m";
         String www="\033[47;30m";
         int x1=0;
-        for(x1=0;x1<xx;x1++)xxx=xxx+" ";
-        for(x1=0;x1<w;x1++)www=www+" ";
+        for(x1=0;x1<xx/8;x1++)xxx=xxx+" ";
+        for(x1=0;x1<w/8;x1++)www=www+" ";
         www=xxx+www;
         System.out.println("\033c\033[47;30m\ninvoke box \n");
-        for(x1=0;x1<y;x1++)System.out.println("");
-        for(x1=0;x1<w;x1++)System.out.println(www);
+        for(x1=0;x1<y/8;x1++)System.out.println("");
+        for(x1=0;x1<h/8;x1++)System.out.println(www);
             
         
 
